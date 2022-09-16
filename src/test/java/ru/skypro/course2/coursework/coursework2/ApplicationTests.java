bb01a383c0eb30917306c7141f5eef4e50b0af97
AHTI6IOTIK <phsts@yandex.ru>
2022-09-16 23:01:34 +0300

    initial commit

---
+++ src/test/java/ru/skypro/course2/coursework/coursework2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.course2.coursework.coursework2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
